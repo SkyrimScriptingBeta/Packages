ed2c8705bccf9dc4c5393b6a9ffb35f8310424f8
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-26 22:55:12 -0800

    Don't remove /lib or /debug

--- ports/example-library/portfile.cmake
+++ ports/example-library/portfile.cmake
@@ -10,10 +10,5 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug"
-    "${CURRENT_PACKAGES_DIR}/lib"
-)
-
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

6da339f35639e2a6d440df513c1d433df5e4c9a7
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-26 18:52:09 -0800

    Add example_header_only_library xmake package

---
+++ packages/e/example_header_only_library/xmake.lua
@@ -0,0 +1,11 @@
+package("example_header_only_library")
+    set_homepage("https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary")
+    set_description("Example header-only C++ library for testing Packages registry")
+    set_license("0BSD")
+
+    add_urls("https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary/archive/refs/tags/v$(version).tar.gz",
+            "https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary.git")
+
+    add_versions("0.0.1", "3A6291CE6AF17A75E2B19A778D12C1E6AD347BD250348DB5AEBB101E085EC58E")
+
+    https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary/archive/refs/tags/v0.0.1.tar.gz

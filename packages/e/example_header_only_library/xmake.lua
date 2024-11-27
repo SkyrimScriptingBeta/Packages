f82e995a32ded56195a26b20262972333e7edae1
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-26 18:59:57 -0800

    Let's not use 'v' in version tags

--- packages/e/example_header_only_library/xmake.lua
+++ packages/e/example_header_only_library/xmake.lua
@@ -3,7 +3,7 @@ package("example_header_only_library")
     set_description("Example header-only C++ library for testing Packages registry")
     set_license("0BSD")
 
-    add_urls("https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary/archive/refs/tags/v$(version).tar.gz",
+    add_urls("https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary/archive/refs/tags/$(version).tar.gz",
             "https://github.com/SkyrimScriptingBeta/ExampleHeaderOnlyLibrary.git")
 
     add_versions("0.0.1", "3A6291CE6AF17A75E2B19A778D12C1E6AD347BD250348DB5AEBB101E085EC58E")

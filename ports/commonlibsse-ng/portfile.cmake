1df0f61357411f8147a364741a87b862ef9a4f8d
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-28 03:10:11 -0800

    Update commonlibsse-ng 2024-09-04-b93280e (2024-11-28 03:10:11)

--- ports/commonlibsse-ng/portfile.cmake
+++ ports/commonlibsse-ng/portfile.cmake
@@ -4,11 +4,21 @@ vcpkg_from_git(
     REF b93280e832f263dbef44e44cbe2936622a02f91a
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+    OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on
 )
 
-vcpkg_cmake_install()
+vcpkg_install_cmake()
+vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSSE CONFIG_PATH lib/cmake)
+vcpkg_copy_pdbs()
 
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE/*.cmake")
+file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
+file(INSTALL "${SOURCE_PATH}/cmake/CommonLibSSE.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE")
+
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

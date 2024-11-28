27d1c14248bd0d777bffe407141fc0412f2aa0bb
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-28 04:31:17 -0800

    Update commonlibsse-1-5-97 2024-11-25-3e29894 (2024-11-28 04:31:17)

--- ports/commonlibsse-1-5-97/portfile.cmake
+++ ports/commonlibsse-1-5-97/portfile.cmake
@@ -4,11 +4,15 @@ vcpkg_from_git(
     REF 3e2989448cbe5a61c17cf5a21abd5d97c51276c3
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+    OPTIONS -DSKYRIM_SUPPORT_AE=off -DSKSE_SUPPORT_XBYAK =on
 )
 
-vcpkg_cmake_install()
+vcpkg_install_cmake()
+vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSSE CONFIG_PATH lib/cmake)
+vcpkg_copy_pdbs()
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

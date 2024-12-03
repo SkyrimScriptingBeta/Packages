776b1db706acf7ba4ccbcc96c4be3b2ead412015
Mrowr Purr <mrowr.purr@gmail.com>
2024-12-02 19:25:23 -0800

    Update commonlibsse-powerof3-latest latest (2024-12-02 19:25:23)

--- ports/commonlibsse-powerof3-latest/portfile.cmake
+++ ports/commonlibsse-powerof3-latest/portfile.cmake
@@ -7,43 +7,20 @@ vcpkg_extract_source_archive(
     ARCHIVE ${DOWNLOADS}/commonlibsse-powerof3-latest.tar.gz
 )
 
-option(REX_OPTION_INI "Enables ini config support for REX." OFF)
-option(REX_OPTION_JSON "Enables json config support for REX." OFF)
-option(REX_OPTION_TOML "Enables toml config support for REX." OFF)
-option(SKSE_SUPPORT_XBYAK "Enables trampoline support for Xbyak." OFF)
-option(SKYRIM_SUPPORT_AE "Enables support for Skyrim AE" OFF)
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS)
-
-if(rex-ini IN_LIST OUT_FEATURE_OPTIONS)
-    message(STATUS "Enabling ini config support for REX.")
-    set(REX_OPTION_INI on)
-endif()
-
-if(rex-json IN_LIST OUT_FEATURE_OPTIONS)
-    message(STATUS "Enabling json config support for REX.")
-    set(REX_OPTION_JSON on)
-endif()
-
-if(rex-toml IN_LIST OUT_FEATURE_OPTIONS)
-    message(STATUS "Enabling toml config support for REX.")
-    set(REX_OPTION_TOML on)
-endif()
-
-if(xbyak IN_LIST OUT_FEATURE_OPTIONS)
-    message(STATUS "Enabling trampoline support for Xbyak.")
-    set(SKSE_SUPPORT_XBYAK on)
-endif()
-
-if(skyrim-ae IN_LIST OUT_FEATURE_OPTIONS)
-    message(STATUS "Enabling support for Skyrim AE.")
-    set(SKYRIM_SUPPORT_AE on)
-endif()
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    rex-ini REX_OPTION_INI
+    rex-json REX_OPTION_JSON
+    rex-toml REX_OPTION_TOML
+    xbyak SKSE_SUPPORT_XBYAK
+    skyrim-ae SKYRIM_SUPPORT_AE
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
-    OPTIONS -DREX_OPTION_INI=${REX_OPTION_INI} -DREX_OPTION_JSON=${REX_OPTION_JSON} -DREX_OPTION_TOML=${REX_OPTION_TOML} -DSKSE_SUPPORT_XBYAK=${SKSE_SUPPORT_XBYAK} -DSKYRIM_SUPPORT_AE=${SKYRIM_SUPPORT_AE}
+    OPTIONS ${FEATURE_OPTIONS}
 )
 
 vcpkg_install_cmake()

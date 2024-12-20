7bd5414198fe4b4d1e911170e6fc1279f92fcdcd
Mrowr Purr <mrowr.purr@gmail.com>
2024-12-20 02:59:27 -0800

    Update skse-plugin-definition 2024-12-03-698ab4d (2024-12-20 02:59:27)

--- ports/skse-plugin-definition/portfile.cmake
+++ ports/skse-plugin-definition/portfile.cmake
@@ -4,11 +4,13 @@ vcpkg_from_git(
     REF 698ab4d7e4ac94d1aad00fb05da7469834fc646b
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-)
-
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

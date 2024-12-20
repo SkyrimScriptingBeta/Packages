8a1d109f197b67439fc582707246ecff5e4b495e
Mrowr Purr <mrowr.purr@gmail.com>
2024-12-20 02:49:00 -0800

    Update cmake-skyrim 2024-12-20-8020754 (2024-12-20 02:49:00)

--- ports/cmake-skyrim/portfile.cmake
+++ ports/cmake-skyrim/portfile.cmake
@@ -4,11 +4,15 @@ vcpkg_from_git(
     REF 80207543c4443590a4274f81510269eae86a61e3
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-)
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME SkyrimCMake CONFIG_PATH lib/cmake)
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include")

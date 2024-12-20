fe715ba6e246cb4db9ed7c05ffc376696f8f98ae
Mrowr Purr <mrowr.purr@gmail.com>
2024-12-20 02:58:08 -0800

    Add new port skse-plugin-definition

---
+++ ports/skse-plugin-definition/portfile.cmake
@@ -0,0 +1,14 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/SkyrimScriptingBeta/skse-plugin-definition.git
+    REF 698ab4d7e4ac94d1aad00fb05da7469834fc646b
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_cmake_install()
+
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

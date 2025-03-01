c5cc350a1b6ccbc1cc05be95eb37ae09f6d05da6
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-01 14:26:47 -0800

    Getting example_static_library to work

--- packages/e/example_static_library/xmake.lua
+++ packages/e/example_static_library/xmake.lua
@@ -6,26 +6,19 @@ package("example_static_library")
     add_urls("https://github.com/SkyrimScriptingBeta/ExampleStaticLibrary/archive/refs/tags/$(version).tar.gz",
             "https://github.com/SkyrimScriptingBeta/ExampleStaticLibrary.git")
 
-    -- Allow users to specify which CommonLib package they want
+    -- Require users to specify which CommonLib package they want
     add_configs("commonlib", {description = "Specify package name for commonlib dependency", default = nil, type = "string"})
 
     on_load(function (package)
         local commonlib = package:config("commonlib")
-        if not commonlib then
+        if commonlib then
+            print("Using CommonLib package: " .. commonlib)
+            package:add("deps", commonlib)
+        else
             raise("You must specify a CommonLib version, e.g., `xmake f --commonlib=skyrim-commonlib-ae`")
         end
-
-        -- Register the user-selected package as a required dependency
-        package:add("deps", commonlib)
     end)
 
     on_install(function (package)
-        local commonlib = package:config("commonlib")
-
-        -- Add the required includes and link directories
-        package:add("includedirs", package:dep(commonlib):installdir("include"))
-        package:add("linkdirs", package:dep(commonlib):installdir("lib"))
-        package:add("links", package:dep(commonlib):get("links"))
-
         import("package.tools.xmake").install(package, {})
     end)

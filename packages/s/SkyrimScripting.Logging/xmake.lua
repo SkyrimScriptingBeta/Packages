3808327dec13dea4bfc0750fb3ecda495a7cb14a
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-02 09:33:04 -0800

    add SkyrimScripting.Logging

--- packages/s/SkyrimScripting.Logging/xmake.lua
+++ packages/s/SkyrimScripting.Logging/xmake.lua
@@ -1,17 +1,20 @@
-package("SkyrimScripting.Entrypoint")
-    set_homepage("https://github.com/SkyrimScriptingBeta/Entrypoint")
-    set_description("Allow multiple libraries to hook into your SKSE plugin entrypoint")
+-- This is an example of a package() which can be shared in any xmake package registry
+
+package("SkyrimScripting.Logging")
+    set_homepage("https://github.com/SkyrimScriptingBeta/Logging")
+    set_description("My library which uses CommonLib")
     set_license("0BSD")
 
-    add_urls("https://github.com/SkyrimScriptingBeta/Entrypoint/archive/refs/tags/$(version).tar.gz",
-            "https://github.com/SkyrimScriptingBeta/Entrypoint.git")
+    add_urls("https://github.com/SkyrimScriptingBeta/Logging/archive/refs/tags/$(version).tar.gz",
+            "https://github.com/SkyrimScriptingBeta/Logging.git")
 
-    add_deps("global_macro_functions")
+    add_deps("_Log_")
 
-    add_configs("commonlib", {description = "Specify package name for commonlib dependency", default = nil, type = "string"})
+    add_configs("commonlib", {description = "Specify package name for commonlib dependency", default = "", type = "string"})
     add_configs("require_commonlib", {description = "If true, add_requires the CommonLib package", default = true, type = "boolean"})
 
-    add_configs("include_repo_skyrimscripting", {description = "Include SkyrimScripting repository during build", default = false, type = "boolean"})
+    add_configs("include_repo_skyrimscripting", {description = "Include SkyrimScripting repository during build", default = true, type = "boolean"})
+    add_configs("include_repo_skyrimscripting_beta", {description = "Include SkyrimScripting repository during build", default = true, type = "boolean"})
     add_configs("include_repo_mrowrlib", {description = "Include MrowrLib repository during build", default = true, type = "boolean"})
 
     add_configs("build_example", {description = "Build example project using this library", default = false, type = "boolean"})
@@ -29,6 +32,7 @@ package("SkyrimScripting.Entrypoint")
         import("package.tools.xmake").install(package, {
             commonlib = package:config("commonlib"),
             include_repo_skyrimscripting = package:config("include_repo_skyrimscripting"),
+            include_repo_skyrimscripting_beta = package:config("include_repo_skyrimscripting_beta"),
             include_repo_mrowrlib = package:config("include_repo_mrowrlib"),
             build_example = package:config("build_example"),
             build_papyrus_scripts = package:config("build_papyrus_scripts"),

bb426f08b15e541e733b96105c5101af55739d62
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-02 03:56:26 -0800

    Make all configs available to configure

--- packages/s/SkyrimScripting.Entrypoint/xmake.lua
+++ packages/s/SkyrimScripting.Entrypoint/xmake.lua
@@ -1,17 +1,22 @@
 package("SkyrimScripting.Entrypoint")
-    set_homepage("https://github.com/SkyrimScriptingBeta/SKSE_Entrypoint")
+    set_homepage("https://github.com/SkyrimScriptingBeta/Entrypoint")
     set_description("Allow multiple libraries to hook into your SKSE plugin entrypoint")
     set_license("0BSD")
 
-    add_urls("https://github.com/SkyrimScriptingBeta/SKSE_Entrypoint/archive/refs/tags/$(version).tar.gz",
-            "https://github.com/SkyrimScriptingBeta/SKSE_Entrypoint.git")
+    add_urls("https://github.com/SkyrimScriptingBeta/Entrypoint/archive/refs/tags/$(version).tar.gz",
+            "https://github.com/SkyrimScriptingBeta/Entrypoint.git")
 
     add_deps("global_macro_functions")
 
-    -- Require users to specify which CommonLib package they want
     add_configs("commonlib", {description = "Specify package name for commonlib dependency", default = nil, type = "string"})
+    add_configs("include_repo_skyrimscripting", {description = "Include SkyrimScripting repository during build", default = false, type = "boolean"})
+    add_configs("include_repo_mrowrlib", {description = "Include MrowrLib repository during build", default = false, type = "boolean"})
+    add_configs("build_example", {description = "Build example project using this library", default = false, type = "boolean"})
+    add_configs("build_papyrus_scripts", {description = "Build Papyrus scripts", default = false, type = "boolean"})
+    add_configs("require_commonlib", {description = "If true, add_requires the CommonLib package", default = true, type = "boolean"})
 
     on_load(function (package)
+        -- Require users to specify which CommonLib package they want
         local commonlib = package:config("commonlib")
         if not commonlib then
             raise("You must specify a CommonLib version, e.g., `xmake f --commonlib=skyrim-commonlib-ae`")
@@ -21,11 +26,10 @@ package("SkyrimScripting.Entrypoint")
     on_install(function (package)
         import("package.tools.xmake").install(package, {
             commonlib = package:config("commonlib"),
-            -- Hardcoded options (disabling options which are 'true' for local development)
-            require_commonlib = false,
-            include_repo_skyrimscripting = false,
-            include_repo_mrowrlib = false,
-            build_example = false,
-            build_papyrus_scripts = false
+            include_repo_skyrimscripting = package:config("include_repo_skyrimscripting"),
+            include_repo_mrowrlib = package:config("include_repo_mrowrlib"),
+            build_example = package:config("build_example"),
+            build_papyrus_scripts = package:config("build_papyrus_scripts"),
+            require_commonlib = package:config("require_commonlib")
         })
     end)

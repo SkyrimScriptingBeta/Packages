486b032fa0c946eca73c6e75726f548e90ee6102
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-15 03:42:54 -0700

    Cleanup SkyrimScripting.Entrypoint

--- packages/s/SkyrimScripting.Entrypoint/xmake.lua
+++ packages/s/SkyrimScripting.Entrypoint/xmake.lua
@@ -1,6 +1,6 @@
 package("SkyrimScripting.Entrypoint")
     set_homepage("https://github.com/SkyrimScriptingBeta/Entrypoint")
-    set_description("Allow multiple libraries to hook into your SKSE plugin entrypoint")
+    set_description("Allows other libraries to hook into your SKSE plugin entrypoint")
     set_license("0BSD")
 
     add_urls("https://github.com/SkyrimScriptingBeta/Entrypoint/archive/refs/tags/$(version).tar.gz",
@@ -8,14 +8,7 @@ package("SkyrimScripting.Entrypoint")
 
     add_deps("global_macro_functions")
 
-    add_configs("commonlib", {description = "Specify package name for commonlib dependency", default = nil, type = "string"})
-    add_configs("require_commonlib", {description = "If true, add_requires the CommonLib package", default = true, type = "boolean"})
-
-    add_configs("include_repo_skyrimscripting", {description = "Include SkyrimScripting repository during build", default = false, type = "boolean"})
-    add_configs("include_repo_mrowrlib", {description = "Include MrowrLib repository during build", default = true, type = "boolean"})
-
-    add_configs("build_example", {description = "Build example project using this library", default = false, type = "boolean"})
-    add_configs("build_papyrus_scripts", {description = "Build Papyrus scripts", default = false, type = "boolean"})
+    add_configs("commonlib", { description = "Specify package name for commonlib dependency", default = "skyrim-commonlib-ng", type = "string" })
 
     on_load(function (package)
         -- Require users to specify which CommonLib package they want
@@ -28,10 +21,6 @@ package("SkyrimScripting.Entrypoint")
     on_install(function (package)
         import("package.tools.xmake").install(package, {
             commonlib = package:config("commonlib"),
-            include_repo_skyrimscripting = package:config("include_repo_skyrimscripting"),
-            include_repo_mrowrlib = package:config("include_repo_mrowrlib"),
-            build_example = package:config("build_example"),
-            build_papyrus_scripts = package:config("build_papyrus_scripts"),
-            require_commonlib = package:config("require_commonlib")
+            build_example = false
         })
     end)

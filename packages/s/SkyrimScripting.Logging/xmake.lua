6ea3972898b83e9125d862031ea2fbb7bd3a173f
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-19 19:54:33 -0700

    Update simplified logging

--- packages/s/SkyrimScripting.Logging/xmake.lua
+++ packages/s/SkyrimScripting.Logging/xmake.lua
@@ -6,21 +6,7 @@ package("SkyrimScripting.Logging")
     add_urls("https://github.com/SkyrimScriptingBeta/Logging/archive/refs/tags/$(version).tar.gz",
             "https://github.com/SkyrimScriptingBeta/Logging.git")
 
-    add_deps("global_macro_functions")
-
     add_configs("commonlib", {description = "Specify package name for commonlib dependency", default = "", type = "string"})
-    add_configs("require_commonlib", {description = "If true, add_requires the CommonLib package", default = true, type = "boolean"})
-
-    add_configs("include_repo_skyrimscripting", {description = "Include SkyrimScripting repository during build", default = true, type = "boolean"})
-    add_configs("include_repo_skyrimscripting_beta", {description = "Include SkyrimScripting repository during build", default = true, type = "boolean"})
-    add_configs("include_repo_mrowrlib", {description = "Include MrowrLib repository during build", default = true, type = "boolean"})
-
-    add_configs("build_example", {description = "Build example project using this library", default = false, type = "boolean"})
-    add_configs("build_papyrus_scripts", {description = "Build Papyrus scripts", default = false, type = "boolean"})
-
-    add_configs("use_skyrimscripting_entrypoint", {description = "If true, builds with support for the SkyrimScripting.Entrypoint library", default = false, type = "boolean"})
-    add_configs("use_log_library", {description = "If true, builds with support for the _Log_ library", default = false, type = "boolean"})
-    add_configs("use_skse_plugin_info_library", {description = "If true, builds with support for the SKSEPluginInfo library", default = false, type = "boolean"})
 
     on_load(function (package)
         -- Require users to specify which CommonLib package they want
@@ -28,42 +14,11 @@ package("SkyrimScripting.Logging")
         if not commonlib then
             raise("You must specify a CommonLib version, e.g., `xmake f --commonlib=skyrim-commonlib-ae`")
         end
-
-        -- If use_log_library, then add the "_Log_" library to package deps:
-        if package:config("use_log_library") then
-            package:add("deps", "_Log_")
-        end
-
-        -- If use_skse_plugin_info_library, then add the "SKSEPluginInfo" library to package deps:
-        if package:config("use_skse_plugin_info_library") then
-            package:add("deps", "skse_plugin_info")
-        end
-
-        if package:config("use_skyrimscripting_entrypoint") then
-            -- [Entrypoint]
-            -- Require SkyrimScripting.Entrypoint, adding to package deps,
-            -- and pass along all of these config options: commonlib, include_repo_skyrimscripting, include_repo_mrowrlib, build_example
-            package:add("deps", "SkyrimScripting.Entrypoint", { configs = {
-                commonlib = commonlib,
-                require_commonlib = package:config("require_commonlib"),
-                include_repo_skyrimscripting = package:config("include_repo_skyrimscripting"),
-                include_repo_mrowrlib = package:config("include_repo_mrowrlib"),
-                build_example = package:config("build_example")
-            }})
-        end
     end)
 
     on_install(function (package)
         import("package.tools.xmake").install(package, {
             commonlib = package:config("commonlib"),
-            include_repo_skyrimscripting = package:config("include_repo_skyrimscripting"),
-            include_repo_skyrimscripting_beta = package:config("include_repo_skyrimscripting_beta"),
-            include_repo_mrowrlib = package:config("include_repo_mrowrlib"),
-            build_example = package:config("build_example"),
-            build_papyrus_scripts = package:config("build_papyrus_scripts"),
-            require_commonlib = package:config("require_commonlib"),
-            use_log_library = package:config("use_log_library"),
-            use_skse_plugin_info_library = package:config("use_skse_plugin_info_library"),
-            use_skyrimscripting_entrypoint = package:config("use_skyrimscripting_entrypoint")
+            build_example = false
         })
     end)

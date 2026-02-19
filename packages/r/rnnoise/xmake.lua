efc6a61a24c3a8299c3f9bde002350276caeefd4
Mrowr Purr <mrowr.purr@gmail.com>
2026-02-19 04:27:50 -0800

    main -> master

--- packages/r/rnnoise/xmake.lua
+++ packages/r/rnnoise/xmake.lua
@@ -3,14 +3,13 @@ package("rnnoise")
     set_description("Recurrent neural network for audio noise reduction")
     set_license("BSD-3-Clause")
 
-    -- Use jmvalin's cmake branch which has pre-trained model and CMake support
-    add_urls("https://github.com/GregorR/rnnoise-cmake.git")
-    add_versions("cmake", "master")
+    -- Use TeaSpeak's CMake fork which has pre-trained model included
+    add_urls("https://github.com/TeaSpeak/rnnoise-cmake.git")
+    add_versions("master", "master")
 
     on_install(function(package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
         import("package.tools.cmake").install(package, configs)
     end)
 

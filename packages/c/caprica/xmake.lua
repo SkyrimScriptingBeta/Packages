938e16f07f3f9b18b34969d291cd9f921fca4fad
Mrowr Purr <mrowr.purr@gmail.com>
2026-02-27 13:22:50 -0800

    Actually fix Caprica

--- packages/c/caprica/xmake.lua
+++ packages/c/caprica/xmake.lua
@@ -10,25 +10,6 @@ package("caprica")
     add_deps("pugixml")
 
     on_install(function(package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            set_languages("c++23")
-
-            add_requires("boost", { configs = { filesystem = true, program_options = true, container = true } })
-            add_requires("pugixml")
-
-            target("caprica")
-                set_kind("static")
-                add_packages("boost", "pugixml", { public = true })
-                add_files("caprica/common/**.cpp")
-                add_files("caprica/papyrus/**.cpp")
-                add_files("caprica/pex/**.cpp")
-                add_includedirs("caprica", { public = true })
-                add_headerfiles("caprica/(common/**.h)")
-                add_headerfiles("caprica/(papyrus/**.h)")
-                add_headerfiles("caprica/(pex/**.h)")
-                add_cxxflags("cl::/Zc:inline", "cl::/bigobj")
-        ]])
         import("package.tools.xmake").install(package)
     end)
 

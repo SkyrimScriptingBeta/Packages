c3518f0613812993670ca0bbbe00adc80b5316d0
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-03 07:38:46 -0800

    wicked-engine changes

--- packages/w/wicked-engine/xmake.lua
+++ packages/w/wicked-engine/xmake.lua
@@ -8,3 +8,19 @@ package("wicked-engine")
     on_install(function(package)
         import("package.tools.xmake").install(package)
     end)
+
+    on_fetch(function(package)
+        local result = {}
+        result.links = {"WickedEngine", "Jolt", "LUA", "Utility"}
+        result.syslinks = {"d3d12", "dxgi", "d3dcompiler", "dxguid", "comdlg32", "shell32"}
+        result.linkdirs = {package:installdir("lib")}
+        result.includedirs = {package:installdir("include")}
+        result.defines = {
+            "WIN32=1", "_HAS_EXCEPTIONS=0", "UNICODE", "_UNICODE", "NOMINMAX", "WICKED_CMAKE_BUILD",
+            "_XM_SSE4_INTRINSICS_", "_XM_AVX_INTRINSICS_", "_XM_F16C_INTRINSICS_", "_XM_FMA3_INTRINSICS_",
+            "JPH_DEBUG_RENDERER",
+            "JPH_USE_SSE4_1", "JPH_USE_SSE4_2", "JPH_USE_AVX",
+            "JPH_USE_LZCNT", "JPH_USE_TZCNT", "JPH_USE_F16C", "JPH_USE_FMADD"
+        }
+        return result
+    end)

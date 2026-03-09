8cdaa1f9d16d847e1303916bddd27f2d0b46d66d
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-08 19:23:26 -0700

    Add graphiti C++ port

---
+++ packages/g/graphiti/xmake.lua
@@ -0,0 +1,17 @@
+package("graphiti")
+    set_homepage("https://github.com/SkyrimScriptingBeta/graphiti")
+    set_description("Temporally-aware knowledge graph library for AI agents")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/SkyrimScriptingBeta/graphiti.git")
+    add_versions("mrowrpurr", "mrowrpurr")
+
+    add_deps("kuzu")
+    add_deps("nlohmann_json")
+    add_deps("cpp-httplib")
+    add_deps("openssl3")
+    add_deps("yaml-cpp")
+
+    on_install(function(package)
+        import("package.tools.xmake").install(package)
+    end)

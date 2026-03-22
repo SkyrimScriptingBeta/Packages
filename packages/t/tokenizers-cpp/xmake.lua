05668649cea625d627f4e340857b84fca0bfa0cd
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-21 23:28:46 -0700

    Update xmake.lua

--- packages/t/tokenizers-cpp/xmake.lua
+++ packages/t/tokenizers-cpp/xmake.lua
@@ -6,6 +6,12 @@ package("tokenizers-cpp")
     add_urls("https://github.com/mrowrpurr/tokenizers-cpp.git")
     add_versions("mrowrpurr", "mrowrpurr")
 
+    if is_plat("windows", "mingw") then
+        add_syslinks("ntdll", "wsock32", "ws2_32", "bcrypt", "iphlpapi", "userenv", "psapi")
+    elseif is_plat("linux") then
+        add_syslinks("dl", "pthread")
+    end
+
     on_install(function(package)
         import("package.tools.xmake").install(package)
     end)
